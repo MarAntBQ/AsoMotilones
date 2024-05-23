1e3d0b09446763bf924ce4dc663f627aeeddfeb4
MarAntBQ <github@marantbq.dev>
2024-05-23 17:09:59 -0500

    Add project

---
+++ includes/head.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+<link rel="stylesheet" href="css/styles.css">
+<link rel="icon" type="image/png" href="favicon.png" />
+<!-- SEO Codes -->
+<meta name="keywords" content="Nos, dedicamos, aprovechamiento, materiales, reciclables, programa, medio, ambiental, sostenible, igualitario, Asociación, de, Recicladores, Los, Motilones, AREMOS, ESP, empresa, cucuteña, creada, grupo, personas, comprometidas, ambiente, busca, solución, real, problemas, residuos, aprovechables, lograr, dignificación, recicladores, oficio">
+<meta name="description" content="Nos dedicamos al aprovechamiento de materiales reciclables a través de un programa medio ambiental, sostenible e igualitario.">
+<meta name="author" content="Marbust Technology Company">
+<meta name="copyright" content="© <?php echo date("Y");?> Marbust Technology Company - All Rights Reserved">
+<script src="js/functions.js"></script>
+<!--https://michalsnik.github.io/aos/-->
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
---
+++ pages/home.php
@@ -0,0 +1,134 @@
+<div class="banner" id="home">
+    <img src="img/banner/logo.png" alt="Aso Motilones Logo">
+    <h1 class="fs70px fs-i-55px fs-m-40px fs-p-30px">AREMOS ESP</h1>
+    <p>Nos dedicamos al aprovechamiento de materiales reciclables a través de un programa medio ambiental, sostenible e igualitario.</p>
+    <a href="./#about"><i class="fas fa-chevron-down"></i></a>
+</div>
+<div class="page-section" id="about">
+    <h1 class="fs40px" data-aos="flip-up" data-aos-duration="800">¿Quiénes Somos?</h1>
+    <hr class="w100px">
+    <div class="flex-s-a flex-vc">
+        <div class="box-6 box-i-5 box-m-8 box-p-10" data-aos="flip-left" data-aos-duration="800">
+            <div class="w90p mauto">
+                <lottie-player src="https://assets1.lottiefiles.com/packages/lf20_cizyirjg.json" background="transparent" speed="0.1" style="width: 100%;" loop autoplay></lottie-player>
+            </div>
+        </div>
+        <div class="box-6 box-i-7 box-m-12" data-aos="flip-right" data-aos-duration="800">
+            <p class="fs18px lh2em taj"><strong><i class="far fa-check-circle"></i></strong> La Asociación de Recicladores Los Motilones, <strong>AREMOS ESP,</strong> es una empresa cucuteña creada por un grupo de personas comprometidas con el medio ambiente que busca una solución real a los problemas de los residuos aprovechables para lograr la dignificación de los recicladores de oficio.</p>
+            <p class="fs18px lh2em taj"><strong><i class="far fa-check-circle"></i></strong> Se encuentra en proceso de formalización en el marco del Decreto 596 del 2016 del Ministerio de Vivienda Ciudad y Territorio.</p>
+        </div>
+    </div>
+</div>
+<div class="page-section" id="brand">
+    <h1 class="fs40px" data-aos="flip-up" data-aos-duration="800">¡Conócenos!</h1>
+    <hr class="w100px">
+    <div class="flex-s-a flex-vc">
+        <div class="brand-box box-4 box-i-6 box-m-8 box-p-12" data-aos="zoom-in" data-aos-duration="800">
+            <p class="icon-ctnir"><i class="fas fa-award"></i></p>
+            <h2>Misión</h2>
+            <p>La Asociación de Recicladores Los Motilones, AREMOS ESP, tiene como misión prestar el servicio público de aseo en el componente de aprovechamiento. Así mismo, la transformación de los materiales reciclables y la construcción de tejido social y humanitario a través de un programa medio ambiental, sostenible e igualitario.</p>
+        </div>
+        <div class="brand-box box-4 box-i-6 box-m-8 box-p-12" data-aos="zoom-in" data-aos-duration="800">
+            <p class="icon-ctnir"><i class="fas fa-eye"></i></p>
+            <h2>Visión</h2>
+            <p>Para el año 2025, la Asociación de Recicladores Los Motilones, AREMOS ESP, será una empresa nacional especializada en la prestación del servicio público de aseo en el componente de aprovechamiento. Además, una empresa líder en el proceso de la transformación de los materiales reciclables.</p>
+        </div>
+        <div class="brand-box box-4 box-i-6 box-m-8 box-p-12" data-aos="zoom-in" data-aos-duration="800">
+            <p class="icon-ctnir"><i class="fas fa-book"></i></p>
+            <h2>Historia</h2>
+            <p>Con el objetivo de organizar al gremio de recicladores se constituye en la ciudad de San José de Cúcuta en el año 2020 la Asociación de Recicladores Los Motilones – Aremos ESP- impulsada por grupos de recicladores, también Jhonny Ochoa, Jeffersson Benavides y Jennifer Cajamarca.</p>
+        </div>
+    </div>
+</div>
+<div class="page-section" id="services">
+    <h1 class="fs40px" data-aos="flip-up" data-aos-duration="800">Servicios</h1>
+    <hr class="w100px">
+    <div class="flex-s-a flex-vc mt25px">
+        <div class="box-6 box-m-12 box-p-12 flex-s-a service-box" data-aos="zoom-out" data-aos-duration="800">
+            <div class="box-3 flex-vc box-i-2 box-m-4 box-p-8">
+                <div class="w100p mauto">
+                <lottie-player src="https://assets6.lottiefiles.com/packages/lf20_lcoullus.json" background="transparent" speed="1" style="width: 100%;" loop autoplay></lottie-player>
+            </div>
+            </div>
+            <div class="box-9 box-i-10 box-m-8 box-p-12 flex-vcc">
+                <h2>Manejo de Materiales</h2>
+                <p class="taj">Prestamos el servicio de recolección, transporte, clasificación, comercialización y aprovechamiento de materiales.</p>
+            </div>
+        </div>
+        <div class="box-6 box-m-12 box-p-12 flex-s-a service-box" data-aos="zoom-out" data-aos-duration="800">
+            <div class="box-3 flex-vc box-i-2 box-m-4 box-p-8">
+                <div class="w100p mauto">
+                <lottie-player src="https://assets8.lottiefiles.com/packages/lf20_V7hzRK.json" background="transparent" speed="0.5" style="width: 100%;" loop autoplay></lottie-player>
+            </div>
+            </div>
+            <div class="box-9 box-i-10 box-m-8 box-p-12 flex-vcc">
+                <h2>Asesorias</h2>
+                <p>Asesoramos a empresas y personas en la reducción del impacto ambiental de los residuos que se generan en la industria y el comercio.</p>
+            </div>
+        </div>
+        <div class="box-6 box-m-12 box-p-12 flex-s-a service-box" data-aos="zoom-out" data-aos-duration="800">
+            <div class="box-3 flex-vc box-i-2 box-m-4 box-p-8">
+                <div class="w100p mauto">
+                <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_pmhz1tar.json" background="transparent" speed="1" style="width: 100%;" loop autoplay></lottie-player>
+            </div>
+            </div>
+            <div class="box-9 box-i-10 box-m-8 box-p-12 flex-vcc">
+                <h2>Campañas</h2>
+                <p>Realizamos campañas educativas de sensibilización y capacitaciones en temas de aprovechamiento de los materiales.</p>
+            </div>
+        </div>
+        <div class="box-6 box-m-12 box-p-12 flex-s-a service-box" data-aos="zoom-out" data-aos-duration="800">
+            <div class="box-3 flex-vc box-i-2 box-m-4 box-p-8">
+                <div class="w100p mauto">
+                <lottie-player src="https://assets3.lottiefiles.com/private_files/lf30_flhopjdx.json" background="transparent" speed="1" style="width: 100%;" loop autoplay></lottie-player>
+            </div>
+            </div>
+            <div class="box-9 box-i-10 box-m-8 box-p-12 flex-vcc">
+                <h2>Certificados</h2>
+                <p>Realizamos la expedición de certificado de aprovechamiento y garantía de la completa destrucción.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<!--<div class="page-section" id="documents">
+    <h1 class="fs40px" data-aos="flip-up" data-aos-duration="800">Documentos</h1>
+    <hr class="w100px">
+    <div class="flex-s-a flex-vc mt15px">
+        <a class="document-box box-2 box-i-4 box-m-6 box-p-8" target="_blank" href="docs/rut.pdf" data-aos="flip-down" data-aos-duration="800">
+            <i class="fas fa-file-pdf iconpdf"></i>
+            <h3>RUT</h3>
+        </a>
+    </div>
+</div>-->
+<div class="page-section" id="contact">
+    <h1 class="fs40px" data-aos="flip-up" data-aos-duration="800">Contáctanos</h1>
+    <hr class="w100px">
+    <div class="flex-s-a flex-vc">
+        <div class="box-4 box-i-6 box-m-7 box-p-10">
+            <div class="w50p mauto maxw250px">
+                <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_CEyvj2.json" background="transparent" speed="1" style="width: 100%;" loop autoplay></lottie-player>
+            </div>
+            <!--<h2>Address</h2>-->
+            <p class="tac">Avenida 1 No. 12 – 51 Torre 2 Apto. 202 Reserva de San Luis<br><strong class="fs20px">Cúcuta Norte de Santander - Colombia</strong></p>
+        </div>
+        <div class="box-4 box-i-6 box-m-7 box-p-10 middle-box">
+           <div class="w50p mauto maxw250px">
+                <lottie-player src="https://assets2.lottiefiles.com/private_files/lf30_tA7sB3.json" background="transparent" speed="1" style="width: 100%;" loop autoplay></lottie-player>
+            </div>
+            <!--<h2>Phone</h2>-->
+            <p class="tac fs20px"><a href="<?php echo $PHLink;?>">+57 (301) 7125 - 007</a></p>
+        </div>
+        <div class="box-4 box-i-6 box-m-7 box-p-10">
+           <div class="w50p mauto maxw250px">
+                <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_hoqqqwvt.json" background="transparent" speed="1" style="width: 100%;" loop autoplay></lottie-player>
+            </div>
+            <!--<h2>Email</h2>-->
+            <p class="tac fs20px fs-p-10px"><a href="<?php echo $ELink;?>">Gerencia@ASOMotilonesESP.com</a></p>
+            <p class="tac fs16px fs-p-15px"><strong class="gray">Peticiones, Quejas,<br>Reclamos, Sugerencias:</strong></p>
+            <p class="tac fs20px fs-p-10px"><a href="<?php echo $ELink2;?>">PQRS@ASOMotilonesESP.com</a></p>
+        </div>
+    </div>
+    <div class="map-container">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d494.0119380554454!2d-72.49933987603107!3d7.885076232642296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e66459fa92b41c9%3A0x103fea29598705ea!2sAv.%201%20%2312-51%2C%20C%C3%BAcuta%2C%20Norte%20de%20Santander%2C%20Colombia!5e0!3m2!1ses-419!2sec!4v1623884098037!5m2!1ses-419!2sec" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" class="place-map"></iframe>
+</div>
+</div>
